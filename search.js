e7251b4d13cc8831c7ba93ced883d6300f9d68df
Mohammed24N <nawiedneda24@icloud.com>
2025-03-23 11:57:45 +0000

    Update search.js

--- search.js
+++ search.js
@@ -4,7 +4,7 @@ function searchFunction() {
 
     let pages = {
         "overview": "overview.html",
-        "game data": "game-data.html",
+        "game data": "PS3-Hard-Drive-Structure.html",
         "system files": "system-files.html",
         "ps3 data acquisition": "PS3-Data-Acquisition.html",
         "artifacts found on a ps3": "Artifacts-found-on-a-PS3.html"
